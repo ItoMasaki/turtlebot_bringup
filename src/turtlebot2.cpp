4d566870c94d38b254d4541c594996ada4009ea0
l1sum <l1sum@icloud.com>
2019-04-16 20:07:07 +0900

    rewrite lambda

--- src/turtlebot2.cpp
+++ src/turtlebot2.cpp
@@ -14,19 +14,18 @@ using namespace std;
 class Turtlebot : public rclcpp::Node {
 	public : Turtlebot()
 		 : Node("Turtlebot") {
-			// Kobuki Device
-
-			// Linux
-			const char *DeviceSpecial = "/dev/ttyUSB0";
-			// macOS
-			// const char *DeviceSpecial = "/dev/ttys001";
-
+			const char* deviceSpecial = "/dev/kobuki";
 			// init kobuki
-			kobuki = createKobuki(KobukiStringArgument("/dev/kobuki"));
+			kobuki = createKobuki(KobukiStringArgument(deviceSpecial));
 
 			// subscriber cmd_vel
 			_cmd_vel = this->create_subscription<geometry_msgs::msg::Twist>(
-					"cmd_vel", bind(&Turtlebot::controleByVelocity, this, _1));
+					"/cmd_vel",
+					[this](geometry_msgs::msg::Twist::SharedPtr msg) {
+						this->controleByVelocity(msg);
+					}
+					//bind(&Turtlebot::controleByVelocity, this, _1)
+					);
 			// publishger
 			// [TODO] create topic to publish odometry
 			// _timer = this->create_publisher
@@ -39,8 +38,8 @@ class Turtlebot : public rclcpp::Node {
 		 // init velocity;
 		 // linear must use 'm_per_sec'
 		 // angular must use 'rad_per_sec'
-		 float linearVelocity = 0;
-		 float angularVelocity = 0;
+		 double linearVelocity = 0;
+		 double angularVelocity = 0;
 
 		 // init _cmd_vel
 		 rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr _cmd_vel;
@@ -52,6 +51,8 @@ class Turtlebot : public rclcpp::Node {
 			 linearVelocity  = msg->linear.x;
 			 angularVelocity = msg->angular.z;
 
+			 cout << linearVelocity << endl;
+
 			 this->kobuki->setTargetVelocity(linearVelocity, angularVelocity);
 		 };
 };
@@ -59,7 +60,9 @@ class Turtlebot : public rclcpp::Node {
 
 int main(int argc, char *argv[]) {
 	rclcpp::init(argc, argv);
-	rclcpp::spin(std::make_shared<Turtlebot>());
+	
+	rclcpp::spin(make_shared<Turtlebot>());
+
 	rclcpp::shutdown();
 
 	return 0;
