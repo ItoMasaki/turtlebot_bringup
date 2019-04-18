64ec590c4289ef8eee45e277af1c26e534ce56c3
itomasaki <l1sum@ito.local>
2019-04-18 12:49:21 +0900

    test odom

--- src/turtlebot2.cpp
+++ src/turtlebot2.cpp
@@ -8,6 +8,7 @@
 #include <std_msgs/msg/string.hpp>
 #include <geometry_msgs/msg/twist.hpp>
 #include <nav_msgs/msg/odometry.hpp>
+#include <sensor_msgs/msg/battery_state.hpp>
 
 using namespace rt_net;
 using namespace std;
@@ -17,34 +18,42 @@ class Turtlebot : public rclcpp::Node {
 	private :
 		// kobuki device special
 		const char* device_special = "/dev/kobuki";
-		// velocity
-		double linear_velocity = 0;
-		double angular_velocity = 0;
 
-		// subscriber
+		// subscriber twist
 		rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_vel;
-		// publisher
-		rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom;
-		rclcpp::TimerBase::SharedPtr timer;
+
+		// publisher battery status
+		rclcpp::Publisher<sensor_msgs::msg::BatteryState>::SharedPtr battery;
+
+		// pos_x
+		double pos_x;
+		double pos_y;
+		double pos_th;
 
 		// init kobuki
 		Kobuki *kobuki;
 
 		// controle by velocity
 		void controleByVelocity(geometry_msgs::msg::Twist::SharedPtr msg) {
-			this->kobuki->setTargetVelocity(msg->linear.x, msg->angular.z);
-		};
+			kobuki->setTargetVelocity(msg->linear.x, msg->angular.z);
+			kobuki->getPose(&pos_x, &pos_y, &pos_th);
 
+			cout << pos_x << endl;
+
+		};
 
 	public :
 		Turtlebot() :
 			Node("Turtlebot") {
+				kobuki = createKobuki(KobukiStringArgument(device_special));
+				// topic to subscribe twist message
 				cmd_vel = this->create_subscription <geometry_msgs::msg::Twist> (
 					"/cmd_vel",
 					[this](geometry_msgs::msg::Twist::SharedPtr msg) {
 						this->controleByVelocity(msg);
 					}
 				);
+
 			};
 };
 
