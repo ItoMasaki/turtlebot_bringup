3a6b0d937797bf356d53c8ba00bc35d88518f76c
itomasaki <l1sum@ito.local>
2019-04-21 23:49:39 +0900

    fix indent

--- launch/minimal.launch.py
+++ launch/minimal.launch.py
@@ -5,18 +5,18 @@ import launch_ros.actions
 
 def generate_launch_description():
 	return LaunchDescription([
-		launch.actions.LogInfo(
-			msg="ROS2 start turtlebot_bringup minimal node."
-		),
+	    launch.actions.LogInfo(
+    	        msg="ROS2 start turtlebot_bringup minimal node."
+	    ),
 
-        launch_ros.actions.Node(
-            package="turtlebot_bringup",
-            node_executable="turtlebot2",
-		),
+            launch_ros.actions.Node(
+                package="turtlebot_bringup",
+                node_executable="turtlebot2",
+	    ),
 
-		launch_ros.actions.Node(
-			package="ydlidar",
-			node_executable="ydlidar_node",
-		),
+	    launch_ros.actions.Node(
+	        package="ydlidar",
+	        node_executable="ydlidar_node",
+	    ),
 
 	])
