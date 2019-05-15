08baba83b30e42e380421875fb48af541973eee0
itomasaki <l1sum@ito.local>
2019-05-15 10:56:39 +0900

    add logger

--- src/turtlebot2.cpp
+++ src/turtlebot2.cpp
@@ -2,7 +2,11 @@
 
 int main(int argc, char *argv[]) {
 	rclcpp::init(argc, argv);
-	rclcpp::spin(make_shared<Turtlebot>());
+
+	auto node = make_shared<Turtlebot>();
+	RCLCPP_INFO(node->get_logger(), "START TURTLEBOT.");
+	
+	rclcpp::spin(node);
 	rclcpp::shutdown();
 
 	return 0;
