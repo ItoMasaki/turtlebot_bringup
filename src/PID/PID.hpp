9387f71f7e4e0c4d0ff305892554c91e72925ee2
l1sum <l1sum@icloud.com>
2019-07-30 23:17:35 +0900

    change path

---
+++ src/PID/PID.hpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+class PID
+{
+    public:
+        PID(float Kp, float Ki, float Kd);
+        ~PID();
+
+        float Kp;
+        float Ki;
+        float Kd;
+
+        float Es = 0;
+        float Ep = 0;
+
+        float system(float R, float Y);
+
+    private:
+        float average(float R, float Y);
+        float integral(float E);
+        float differential(float E);
+
+};
