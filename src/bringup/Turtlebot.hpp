04bcf61748f54b66d7ee3da35403783c185b875b
ItoMasaki <l1sum@icloud.com>
2019-11-08 08:35:00 +0900

    add tf broadcaster

--- src/bringup/Turtlebot.hpp
+++ src/bringup/Turtlebot.hpp
@@ -2,9 +2,12 @@
 
 #include <rclcpp/rclcpp.hpp>
 
+#include <tf2_ros/transform_broadcaster.h>
+
 #include <std_msgs/msg/string.hpp>
 #include <std_msgs/msg/bool.hpp>
 #include <geometry_msgs/msg/twist.hpp>
+#include <geometry_msgs/msg/transform_stamped.hpp>
 #include <geometry_msgs/msg/quaternion.hpp>
 #include <nav_msgs/msg/odometry.hpp>
 #include <sensor_msgs/msg/battery_state.hpp>
@@ -40,12 +43,15 @@ class Turtlebot :
     
             // init subscription
             rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr velocity;
-	    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr reset;
+            rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr reset;
     
             // init publisher
             rclcpp::Publisher<sensor_msgs::msg::BatteryState>::SharedPtr battery;
             rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom;
             rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr inertial;
+            
+            // transform broadcaster
+            std::shared_ptr<tf2_ros::TransformBroadcaster> odom_broadcaster;
     
             // init timer
             rclcpp::TimerBase::SharedPtr odometryTimer;
@@ -66,6 +72,10 @@ class Turtlebot :
             double O_position_x = 0;
             double O_position_y = 0;
             double O_orientation_theta = 0;
+
+            double heading;
+            double heading_offset = 0.0/0.0;
+            double head_angle;
     
             // seconds
             double millisec;
@@ -75,6 +85,7 @@ class Turtlebot :
       
             // translate_coordinate
             geometry_msgs::msg::Quaternion translateCoordinate(double x, double y, double z);
+            geometry_msgs::msg::TransformStamped odom_trans;
     
             // init odom
             nav_msgs::msg::Odometry odom_msg = nav_msgs::msg::Odometry();
