335fe51a59b7743899e99b4c71dcc837e8829f02
ItoMasaki <l1sum@icloud.com>
2020-01-13 01:30:20 +0900

    add parameter xml file

--- launch/minimal.launch.py
+++ launch/minimal.launch.py
@@ -1,3 +1,5 @@
+from os import path
+
 from launch import LaunchDescription
 
 import launch.actions
@@ -7,18 +9,19 @@ def generate_launch_description():
     return LaunchDescription([
         launch.actions.LogInfo(
     	    msg="ROS2 start turtlebot_bringup minimal node."
-	),
+    	),
 
         launch_ros.actions.Node(
             package="ydlidar",
             node_executable="ydlidar_node",
             output="screen",
-    ),
+            parameters=["ydlidar.yaml"]
+        ),
 
         launch_ros.actions.Node(
             package="turtlebot_bringup",
             node_executable="turtlebot2",
-			output="screen",
-	),
-
+            output="screen",
+            parameters=["turtlebot2.yaml"]
+        ),
     ])
